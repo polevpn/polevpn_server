055a58c47f20df1b0b1babfc3c6b332c56ec643c
polevpn <polevpn@gmail.com>
2020-12-01 02:26:24 +0000

    support token auth

--- local_login_check.go
+++ local_login_check.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
+const (
+	MAX_PASSWORD_LENGHT = 16
+)
+
+type User struct {
+	Uid           uint64
+	Email         string
+	Vip           int
+	VipExpireTime uint64
+	LastLoginTime uint64
+}
+
 type LocalLoginChecker struct {
 }
 
@@ -8,6 +25,15 @@ func NewLocalLoginChecker() *LocalLoginChecker {
 }
 
 func (llc *LocalLoginChecker) CheckLogin(user string, pwd string) bool {
+
+	if len(pwd) > MAX_PASSWORD_LENGHT {
+		return llc.checkLoginByToken(user, pwd)
+	} else {
+		return llc.checkLoginByPassword(user, pwd)
+	}
+}
+
+func (llc *LocalLoginChecker) checkLoginByToken(user string, pwd string) bool {
 	users := Config.Get("users").AsArray()
 
 	for _, u := range users {
@@ -19,4 +45,27 @@ func (llc *LocalLoginChecker) CheckLogin(user string, pwd string) bool {
 		}
 	}
 	return false
+}
+
+func (llc *LocalLoginChecker) checkLoginByPassword(user string, pwd string) bool {
+
+	crypted, err := base64.StdEncoding.DecodeString(pwd)
+
+	if err != nil {
+		return false
+	}
+
+	origin, err := AesDecrypt(crypted, ServerAesKey)
+	if err != nil {
+		return false
+	}
+
+	userinfo := User{}
+	json.Unmarshal(origin, &userinfo)
+
+	if user != userinfo.Email {
+		return false
+	}
+
+	return true
 }
